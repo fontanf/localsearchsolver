0acbdb01c1f1dbc0bea88c88f1d8f8837e49c53b
Florian Fontan <dev@florian-fontan.fr>
2022-12-10 09:34:00 +0100

    Update .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -5,20 +5,30 @@ def Settings(**kwargs):
                 '-Wall', '-Wextra', '-Werror',
                 '-DGUROBI_FOUND',
                 '-DCPLEX_FOUND',
-                '-DIL_STD', # Cplex
-                '-I', '.',
-                '-I', './bazel-localsearchsolver/',
-                '-I', './bazel-localsearchsolver/external/json/single_include/',
-                '-I', './bazel-localsearchsolver/external/googletest/googletest/include/',
-                '-I', './bazel-localsearchsolver/external/boost/',
-                '-I', './bazel-localsearchsolver/external/simdjson/simdjson-0.9.2/singleheader',
-                '-I', './bazel-localsearchsolver/external/optimizationtools/',
-                # '-I', './../optimizationtools/',
-                '-I', './bazel-localsearchsolver/external/orproblems/',
-                # '-I', './../orproblems/',
+
+                '-DIL_STD',  # Cplex
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/concert/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cplex/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cpoptimizer/include/',
                 '-I', '/home/florian/Programmes/gurobi811/linux64/include/',
+
+                '-I', '.',
+                '-I', './bazel-localsearchsolver/',
+                '-I', './bazel-localsearchsolver/external/'
+                'json/single_include/',
+                '-I', './bazel-localsearchsolver/external/'
+                'googletest/googletest/include/',
+                '-I', './bazel-localsearchsolver/external/'
+                'boost/',
+                '-I', './bazel-localsearchsolver/external/'
+                'simdjson/simdjson-0.9.2/singleheader',
+
+                '-I', './bazel-localsearchsolver/external/'
+                # '-I', './../',
+                'optimizationtools/',
+
+                '-I', './bazel-localsearchsolver/external/'
+                # '-I', './../',
+                'orproblems/',
                 ],
             }
